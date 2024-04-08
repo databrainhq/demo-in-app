6bf27361063a6787239175ce163290683e181d0d
ishaC02 <isha@usedatabrain.com>
2024-04-08 14:00:22 +0530

    feat: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import htmlPluginOpt from "./src/html-configs";
-import htmlPlugin from "vite-plugin-html-config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), htmlPlugin(htmlPluginOpt)],
+  plugins: [react()],
 });
